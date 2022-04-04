a52895580e27aacc5763d49f9325830c5c61456c
Johanna H <92253630+HdlJohanna@users.noreply.github.com>
2022-04-04 18:51:46 +0200

    Create crypto.py

---
+++ naclpy/crypto.py
@@ -0,0 +1,16 @@
+from cryptography.fernet import Fernet
+
+def encrypt(message: bytes, key: bytes) -> bytes:
+    return Fernet(key).encrypt(message)
+
+def decrypt(token: bytes, key: bytes) -> bytes:
+    return Fernet(key).decrypt(token)
+
+def make_key() -> bytes:
+    """
+    The make_key function returns a random key.
+    
+    :return: The sha256 hash of the input string.
+        """
+    
+    return Fernet.generate_key()
