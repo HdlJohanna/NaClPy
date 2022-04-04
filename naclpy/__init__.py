7f08638c571c954ca8ff5e1d7ca19bde5271517b
Johanna H <92253630+HdlJohanna@users.noreply.github.com>
2022-04-04 18:54:02 +0200

    Create __init__.py

---
+++ naclpy/__init__.py
@@ -0,0 +1,19 @@
+"""
+Networking Client-Server Library with a bit of cryptography
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A basic Networking Library to send data from client to server and back.
+
+:copyright: (c) 2022-present HdlJohanna
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'discord'
+__author__ = 'HdlJohanna'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present HdlJohanna'
+__version__ = '0.02a'
+
+from .client import *
+from .server import *
