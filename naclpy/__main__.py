27665f9ddb8797e084810cda49dd92f55d3594b8
Johanna H <92253630+HdlJohanna@users.noreply.github.com>
2022-04-04 18:54:29 +0200

    Create __main__.py

---
+++ naclpy/__main__.py
@@ -0,0 +1,32 @@
+print("""
+# NaClPy
+
+[![Analyze Status](https://github.com/hdljohanna/naclpy/workflows/Analyze/badge.svg?event=push)](https://github.com/nextcord/nextcord-ext-ipc/actions?query=workflow%3AAnalyze+event%3Apush)
+
+## About
+
+A Python extension for Client-Server Networking and (A Bit of) Cryptography.
+
+## Installation
+
+**Python 3.8 or higher is required**
+Install the extension by running one of the following commands:
+
+```py 
+# Linux/macOS
+python3 -m pip install -U git+https://github.com/HdlJohanna/naclpy
+
+# Windows
+py -3 -m pip install -U git+https://github.com/HdlJohanna/naclpy
+```
+
+## Links 
+
+- [Documentation](https://github.com/HdlJohanna/naclpy/wiki)
+- [Usage Examples](https://github.com/HdlJohanna/naclpy/wiki#Samples)
+
+## License
+
+Copyright (c) 2022 Johanna H.
+
+""")
